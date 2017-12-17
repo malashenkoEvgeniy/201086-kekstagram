9b3e1c9718a979105f6d8473ee316adcd88059a6
malashenkoEvgeniy <malashenko_evgeniy@mail.ua>
2017-12-17 20:18:28 +0200

    исправил

--- js/initialize-scale.js
+++ js/initialize-scale.js
@@ -1,7 +1,7 @@
 'use strict';
 (function () {
   window.initializeScale = function (elem, callback) {
-    var scale = parseInt(elem.value);
+    var scale = parseInt(elem.value, 10);
     if (typeof callback === 'function') {
       callback(scale);
     }
